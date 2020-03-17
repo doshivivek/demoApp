73af367796c7babcdd5fbab804083b4f2e791538
VivekDoshi <vdoshi1994@gmail.com>
2020-03-17 21:40:43 +0530

    Updates

---
+++ precache-manifest.e489f1634bf987c4b28a705a49c26fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3d84ae1fbd41cc43714e9dac5e2f4a",
+    "url": "/doshivivek/demoApp.git/index.html"
+  },
+  {
+    "revision": "cff75ead57365f58ce09",
+    "url": "/doshivivek/demoApp.git/static/css/main.26a01724.chunk.css"
+  },
+  {
+    "revision": "38cb8080ef6369ddd26f",
+    "url": "/doshivivek/demoApp.git/static/js/2.188c72c8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/doshivivek/demoApp.git/static/js/2.188c72c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff75ead57365f58ce09",
+    "url": "/doshivivek/demoApp.git/static/js/main.57f21737.chunk.js"
+  },
+  {
+    "revision": "d663df0339f0d7f5b529",
+    "url": "/doshivivek/demoApp.git/static/js/runtime-main.7368e816.js"
+  }
+]);
